9182e22bec1df72df4c23ad32a192c624545d8ec
Sakshi Mittal <sakshi.mittal@knoldus.com>
2021-03-07 21:13:59 +0530

    Concate strings and covert to uppercase

---
+++ Lambda/src/main/java/com/lambda/kup/StringOperations.java
@@ -0,0 +1,14 @@
+package com.lambda.kup;
+
+public class StringOperations  {
+
+        public static void main(String[] args) {
+
+                StringOperations_Interface obj;
+
+                obj = (word1, word2, word3, word4, word5, word6, word7) -> ("The" + "lambda" + "has" + "too" + "many" + "string" + "arguments.")
+                        .toUpperCase();
+                System.out.println(obj.getConcateUppercase("The", "lambda", "has", "too", "many", "string", "arguments."));
+
+        }
+}
---
+++ Lambda/src/main/java/com/lambda/kup/StringOperations_Interface.java
@@ -0,0 +1,7 @@
+package com.lambda.kup;
+
+@FunctionalInterface
+public interface StringOperations_Interface {
+
+        String getConcateUppercase(String word1, String word2, String word3, String word4, String word5, String word6, String word7);
+}
